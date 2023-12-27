15339d0104a35a1eea85d6c1b96016d49c0f5509
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2023-12-27 23:22:52 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+    // image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // render
+
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << '\n'
+              << "255\n";
+
+    for (int j = 0; j < image_height; j++)
+    {
+        for (int i = 0; i < image_width; i++)
+        {
+            double r = double(i) / (image_width - 1);
+            double g = double(j) / (image_width - 1);
+            double b = 0.0;
+
+            int ir = static_cast<int>(255 * r);
+            int ig = static_cast<int>(255 * g);
+            int ib = static_cast<int>(255 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    return 0;
+}
