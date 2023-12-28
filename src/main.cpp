bc57c955b3160880464bf58eb4ba08027d2b735a
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2023-12-28 13:14:20 +0100

    sphere normals

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,34 @@
 #include "ray.h"
 #include "color.h"
 
-bool hit_sphere(const point3 &center, double radius, const ray &r)
+double hit_sphere(const point3 &center, double radius, const ray &r)
 {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray &r)
 {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    double t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+    if (t > 0)
     {
-        return color(1, 0, 0);
+        vec3 N = unit_vector(r.at(t) - point3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
 
     double a = 0.5 * (unit_direction.y() + 1.0);
@@ -32,7 +44,7 @@ int main()
     // image
 
     double aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    int image_width = 800;
 
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
