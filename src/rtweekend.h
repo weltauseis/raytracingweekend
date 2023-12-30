5eb731ed40856bd769b22dc648844754e95df317
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2023-12-30 15:13:34 +0100

    (8.1) random numbers

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // usings
 
@@ -21,6 +22,20 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max)
+{
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
 // common headers
