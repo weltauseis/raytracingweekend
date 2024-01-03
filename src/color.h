ebcd9960ff79a96f45329d1a340f37ef3b4fafc9
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2024-01-03 19:50:11 +0100

    9.5 gamma correction

--- src/color.h
+++ src/color.h
@@ -6,6 +6,11 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    return sqrt(linear_component);
+}
+
 void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
 {
     double r = pixel_color.x();
@@ -16,6 +21,10 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
     r *= scale;
     g *= scale;
     b *= scale;
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const interval intensity(0.000, 0.999);
 
